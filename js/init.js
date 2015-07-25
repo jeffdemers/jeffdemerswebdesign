13081acb81f34e0a6aa2d81c17613ee1c3edf071
jeffdemers <jeffdemers@gmail.com>
2015-07-25 10:59:58 -0700

    parallax working adding form

--- js/init.js
+++ js/init.js
@@ -1,8 +1,8 @@
 (function($){
   $(function(){
 
-    $('.button-collapse').sideNav();
-    $('.parallax').parallax();
+    $('.button-collapse');
+    $('.parallax');
 
   }); // end of document ready
 })(jQuery); // end of jQuery name space
